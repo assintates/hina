740918fc57abc6268d690469783205faa03c2f95
Matsujo Hibiki <assintates@gmail.com>
2020-12-04 00:52:26 +0530

    Fix search.js cors

---
+++ pages/g.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="container">
+    <vue-plyr :options="options">
+      <video
+        controls
+        crossorigin
+        playsinline
+        data-poster="http://proxy.ixil.cc/prox?image=https://www.mymypic.net/data/attachment/forum/202009/13/154847qkaknt11leedhlos.jpg"
+      >
+        <source v-for="(itemd, indexd) in videos"
+                :key="indexd"
+                :size="itemd.quality"
+                :src="GetProxy(itemd.trap)"
+                type="video/mp4"
+        />
+      </video>
+    </vue-plyr>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'g',
+  data() {
+    return {}
+  },
+  asyncData({ params }) {
+    return axios.get(`https://proxy.ixil.cc/lia/functions/videos/phcdn?id=ph5f47b7e9d7fc0`)
+      .then((res) => {
+        return { videos: res.data }
+      })
+  },
+  beforeMount() {
+  },
+
+  mounted() {
+  },
+  methods: {
+    GetProxy(key) {
+      return key.replace('https://dv.phncdn.com/', 'https://proxy.ixil.cc/bpg/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 30%;
+
+  .video-player-box {
+    min-height: 200px;
+  }
+}
+</style>
